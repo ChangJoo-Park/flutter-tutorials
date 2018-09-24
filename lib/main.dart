48a07001e257cfb6b2ffde6ee24a2efd864dd7c5
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-24 23:02:02 +0900

    Remove new keyword from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:routing_app/routes.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       routes: routes,
