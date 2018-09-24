c71db5ec11b833d2517779654014694ceb09fee8
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-24 21:50:45 +0900

    Add routes for pages

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+import 'package:routing_app/pages/app.dart';
+import 'package:routing_app/pages/signup.dart';
+import 'package:routing_app/pages/login.dart';
+
+final routes = {
+  '/': (BuildContext context) => new AppPage(),
+  '/login': (BuildContext context) => new LoginPage(),
+  '/signup': (BuildContext context) => new SignUpPage(),
+};
