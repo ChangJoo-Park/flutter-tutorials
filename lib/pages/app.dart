612e6447d7760d48e9dfecb9e837f85035f2fd38
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-24 21:49:29 +0900

    Add scaffold for pages

---
+++ lib/pages/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new AppPageState();
+  }
+}
+
+class AppPageState extends State<AppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new LoginPageState();
+  }
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
