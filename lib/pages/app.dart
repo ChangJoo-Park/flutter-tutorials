bd7340ec20d0e5e436adb1166b5d7faf736aea55
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-24 22:21:45 +0900

    Implement App Page

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -10,6 +10,27 @@ class AppPage extends StatefulWidget {
 class AppPageState extends State<AppPage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('플러터 라우팅'),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Text(
+            '앱 페이지',
+            style: TextStyle(fontSize: 30.0),
+          ),
+          new RaisedButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/login',
+              );
+            },
+            child: new Text('로그인 페이지로'),
+          ),
+        ],
+      ),
+    );
   }
 }
