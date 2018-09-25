e01b2fd1a1baff322d1d055e9830016ff9a42223
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-25 15:52:57 +0900

    Add button for master-details

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -8,13 +8,22 @@ class FirstPage extends StatelessWidget {
         title: Text('첫번째 페이지'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('두번째 페이지로'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
-        ),
-      ),
+          child: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('두번째 페이지로'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/second');
+            },
+          ),
+          RaisedButton(
+            child: Text('리스팅 페이지로'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/listing');
+            },
+          )
+        ],
+      )),
     );
   }
 }
