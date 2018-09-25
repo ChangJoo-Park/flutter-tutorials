9d5c1d425842ba07924cec0210143d92a0ead081
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-25 15:53:21 +0900

    Add listing routes for master-detail

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:routing_app/pages/first.dart';
 import 'package:routing_app/pages/second.dart';
+import 'package:routing_app/pages/listings/master.dart';
+import 'package:routing_app/pages/listings/detail.dart';
 
 final routes = {
   '/': (BuildContext context) => FirstPage(),
   '/first': (BuildContext context) => FirstPage(),
   '/second': (BuildContext context) => SecondPage(),
+  '/listing': (BuildContext context) => ListingMasterPage(),
 };
